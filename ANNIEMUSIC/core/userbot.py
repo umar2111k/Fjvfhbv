c4db0ca59f2fbc06d1e7207c94955966cca60eef
Umar565 <titaniumgamingr@gmail.com>
2025-01-12 16:35:03 +0530

    Update userbot.py

--- ANNIEMUSIC/core/userbot.py
+++ ANNIEMUSIC/core/userbot.py
@@ -65,10 +65,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("about_ur_moonshining")
-                await self.one.join_chat("kittyxupdates")
-                await self.one.join_chat("grandxmasti")
-                await self.one.join_chat("Naruto_Shippuden_hindi_dubbed_x")
+                await self.one.join_chat("Spark_Developer_Bots")
+                await self.one.join_chat("studyocean1")
+                await self.one.join_chat("Moviez_Backupp")
+                await self.one.join_chat("Spark_Developer")
             except:
                 pass
             assistants.append(1)
@@ -93,10 +93,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("about_ur_moonshining")
-                await self.two.join_chat("kittyxupdates")
-                await self.two.join_chat("grandxmasti")
-                await self.two.join_chat("Naruto_Shippuden_hindi_dubbed_x")
+                await self.two.join_chat("Spark_Developer_Bots")
+                await self.two.join_chat("studyocean1")
+                await self.two.join_chat("Moviez_Backupp")
+                await self.two.join_chat("Spark_Developer")
             except:
                 pass
             assistants.append(2)
@@ -121,10 +121,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("about_ur_moonshining")
-                await self.three.join_chat("kittyxupdates")
-                await self.three.join_chat("grandxmasti")
-                await self.three.join_chat("Naruto_Shippuden_hindi_dubbed_x")
+                await self.three.join_chat("Spark_Developer_Bots")
+                await self.three.join_chat("studyocean1")
+                await self.three.join_chat("Moviez_Backupp")
+                await self.three.join_chat("Spark_Developer")
             except:
                 pass
             assistants.append(3)
@@ -149,10 +149,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("about_ur_moonshining")
-                await self.four.join_chat("kittyxupdates")
-                await self.four.join_chat("grandxmasti")
-                await self.four.join_chat("Naruto_Shippuden_hindi_dubbed_x")
+                await self.four.join_chat("Spark_Developer_Bots")
+                await self.four.join_chat("studyocean1")
+                await self.four.join_chat("Moviez_Backupp")
+                await self.four.join_chat("Spark_Developer")
             except:
                 pass
             assistants.append(4)
@@ -177,10 +177,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("about_ur_moonshining")
-                await self.five.join_chat("kittyxupdates")
-                await self.five.join_chat("grandxmasti")
-                await self.five.join_chat("Naruto_Shippuden_hindi_dubbed_x")
+                await self.five.join_chat("Spark_Developer_Bots")
+                await self.five.join_chat("studyocean1")
+                await self.five.join_chat("Moviez_Backupp")
+                await self.five.join_chat("Spark_Developer")
             except:
                 pass
             assistants.append(5)
