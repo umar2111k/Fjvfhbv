a9c91815160595010d04c1c5839bb32c0918a336
Umar565 <titaniumgamingr@gmail.com>
2025-01-12 18:09:23 +0530

    Update queue.py

--- ANNIEMUSIC/utils/inline/queue.py
+++ ANNIEMUSIC/utils/inline/queue.py
@@ -81,7 +81,7 @@ def aq_markup(_, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/kittyxupdates",
+                url="https://t.me/Spark_Developer_Bots",
             ),
         ],
     ]
@@ -116,7 +116,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/kittyxupdates",
+                url="https://t.me/Spark_Developer_Bots",
             ),
         ],
     ]
